1408974b00944d818e412fdd7cf553aadc88f2d4
edwin <irungundigirigi@gmai.com>
2024-05-15 21:48:11 +0300

    mongodb setup

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${HOST}:${PORT}`;
+
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const users = this.db.collection('users');
+    const user_number = await users.countDocuments();
+    return user_number;
+  }
+
+  async nbFiles() {
+    const files = this.db.collection('files');
+    const file_number = await files.countDocuments();
+    return file_number;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
